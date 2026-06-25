d3091a1cab238a109b7529f1a39a90443894efa0
makihara <makihara@rt-net.jp>
2026-06-25 16:38:50 +0900

    スケールを戻すACC_Saale2は1/4にする

--- STM32duino_sample/RT-9DOF-IMU-V3-Quat/RT-9DOF-IMU-V3-Quat.ino
+++ STM32duino_sample/RT-9DOF-IMU-V3-Quat/RT-9DOF-IMU-V3-Quat.ino
@@ -482,7 +482,7 @@ ICM_20948_Status_e ICM_20948::initializeDMP(void) {
   if (result > worstResult)
     worstResult = result;  // Write accScale to ACC_SCALE DMP register
   // In order to output hardware unit data as configured FSR write 0x00040000 when FSR is 4g
-  const unsigned char accScale2[4] = { 0x00, 0x10, 0x00, 0x00 };
+  const unsigned char accScale2[4] = { 0x00, 0x01, 0x00, 0x00 };
   result = writeDMPmems(ACC_SCALE2, 4, &accScale2[0]);
   if (result > worstResult)
     worstResult = result;  // Write accScale2 to ACC_SCALE2 DMP register
